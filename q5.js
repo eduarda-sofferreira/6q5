97685acf04097306ec3340bcaa477443be6e776a
eduarda-sofferreira <espf1@aluno.ifal.edu.br>
2024-12-08 13:56:16 -0300

    Create q5.js

---
+++ q5.js
@@ -0,0 +1,8 @@
+var c = 1
+var soma = 0
+while ( c <= 10) {
+    var numero = parseInt (prompt ("informe um número"))
+    soma += numero
+    c++
+}
+    console.log("A soma dos números fornecidos é" + soma)
